3bfd82adec732effc8e1e01de2c1b46b80bb476b
rodrig20 <101592068+rodrig20@users.noreply.github.com>
2026-09-09 15:50:43 +0100

    feat(home-manager): configure backup file extension

--- flake.nix
+++ flake.nix
@@ -78,6 +78,7 @@
               {
                 home-manager.useGlobalPkgs = true;
                 home-manager.useUserPackages = true;
+                home-manager.backupFileExtension = ".bak";
                 home-manager.extraSpecialArgs = { inherit inputs; };
               }
 
